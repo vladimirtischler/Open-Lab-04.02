6dc444442347163293a5353b18e12d7ea66e3320
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-18 20:25:37 +0100

    vlado

---
+++ Open-Lab-04.02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Open_Lab_04._02
+{
+    class Program
+    {
+        public static string DoubleChar(string original)
+        {
+            string a = "";
+            for (int i = 0;i < original.Length;i++)
+            {
+                for (int c = 0;c < 2;c++)
+                {
+                    a = a + original[i];
+                }
+            }
+            return a;
+        }
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            Console.WriteLine(DoubleChar(word));
+        }
+    }
+}
